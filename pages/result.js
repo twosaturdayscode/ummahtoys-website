d93151cf803e5aef6b88778f29fa34fdc3384949
alisei <ali.sbtan96@gmail.com>
2022-04-05 23:08:58 +0200

    Improved result page

--- pages/result.js
+++ pages/result.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Result() {
@@ -6,11 +7,35 @@ export default function Result() {
 
   if (status === "success")
     return (
-      <div>
-        <h1> YES! il tuo ordine è</h1>
-        <span>{order}</span>
-        <span>e la tua mail è</span>
-        <span>{email}</span>
+      <div className="w-full flex flex-col items-center my-16 text-zinc-700 gap-3 px-2">
+        <h1 className="font-[CeraPro] font-black text-5xl px-10 text-center">
+          🎉 Abbiamo ricevuto il tuo ordine!
+        </h1>
+        <div className="flex gap-2 mt-5">
+          <span className="text-zinc-600 text-xl">
+            Il numero del tuo ordine è:
+          </span>
+          <span className="font-black text-2xl">#{order}</span>
+        </div>
+        <p className="text-center px-5 md:w-1/2 text-zinc-800">
+          Alhamdulillah, il pagamento è avvenuto con successo, ti abiamo mandato
+          una mail con il resoconto del tuo ordine, all&apos;indirizzo:{" "}
+          <span className="font-black text-lg">{email}</span>
+          <br />
+          <br />
+          In sha&apos;Allah, appena spediremo i tuoi giochi ti invieremo una
+          mail con il numero di tracking del corriere, così saprai esattamente
+          quando arriverà il tuo pacco.
+        </p>
+        <p className="text-center px-5 md:w-1/2 text-sm">
+          L&apos;indirizzo mail è sbagliato? Nessun problema scrivici una mail
+          indicando il tuo numero ordine: ummahtoys@gmail.com
+        </p>
+        <Link href={"/store"}>
+          <a className="w-64 h-16 flex justify-center items-center bg-zinc-700 text-white cursor-pointer focus:ring-4 focus:ring-zinc-300 focus:rounded active:bg-zinc-600">
+            Torna al negozio
+          </a>
+        </Link>
       </div>
     );
 
