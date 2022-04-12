b88e2d00e4212734b41671552e5b062f79d7e77f
alisei <ali.sbtan96@gmail.com>
2022-04-12 16:21:42 +0200

    Aggiunge pagina chi siamo

---
+++ pages/chi-siamo.js
@@ -0,0 +1,30 @@
+export default function Chisiamo() {
+  return (
+    <>
+      <main className="grid grid-cols-12 grid-flow-row w-full my-10 mb-16 gap-8">
+        <h1 className="col-span-full col-start-2 col-end-12 text-5xl text-zinc-700 font-[CeraPro] text-center md:text-left">
+          Chi siamo
+        </h1>
+        <p className="col-span-full col-start-2 col-end-12 text-xl">
+          L’idea nasce a <strong>Maggio 2021</strong> con un obiettivo preciso:
+          avvicinare i bambini musulmani alla religione in modo divertente, da
+          qui il nostro motto: “Imparare l’Islam giocando”.
+          <br />
+          Ummah Toys ha introdotto nel mercato nazionale giocattoli unici e
+          inediti diventando la prima a
+          <strong>
+            produrre e commercializzare giochi di qualità in italiano per
+            bambini musulmani
+          </strong>.
+          <br />
+          Con l’ambizione dei suoi tre fondatori, Ummah Toys vuole essere
+          presente nelle camerette di ogni bambino e bambina musulmani. Tutti i
+          giocattoli sono certificati o prodotti secondo la normativa europea,
+          Ummah Toys ha deciso di mantenere un prezzo accessibile senza
+          compromettere la qualità, perché l’educazione dei nostri figli non è
+          un gioco!
+        </p>
+      </main>
+    </>
+  );
+}
