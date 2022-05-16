fa2e71d9a306ce50e0e6b2dffeed1821ffcc8844
alisei <ali.sbtan96@gmail.com>
2022-05-16 09:41:58 +0200

    update about us

--- pages/chi-siamo.js
+++ pages/chi-siamo.js
@@ -1,28 +1,33 @@
 export default function Chisiamo() {
   return (
     <>
-      <main className="grid grid-cols-12 grid-flow-row w-full my-10 mb-16 gap-8">
+      <main className="grid grid-cols-12 grid-flow-row w-full my-10 mb-16 gap-10">
         <h1 className="col-span-full col-start-2 col-end-12 text-5xl text-zinc-700 font-[CeraPro] text-center md:text-left">
           Chi siamo
         </h1>
-        <p className="col-span-full col-start-2 col-end-12 text-xl">
-          L’idea nasce a <strong>Maggio 2021</strong> con un obiettivo preciso:
-          avvicinare i bambini musulmani alla religione in modo divertente, da
-          qui il nostro motto: “Imparare l’Islam giocando”.
+        <p className="col-span-full col-start-2 col-end-12 text-xl font-light max-w-4xl mx-auto">
+          Ummah Toys nasce nel Maggio 2021 con un obiettivo preciso:{" "}
+          <strong>
+            avvicinare i bambini musulmani alla religione in modo divertente,
+          </strong>{" "}
+          da qui il nostro motto:{" "}
+          <span className="underline decoration-sky-500 decoration-2">
+            Imparare l’Islam giocando.
+          </span>
+          <br />
           <br />
           Ummah Toys ha introdotto nel mercato nazionale giocattoli unici e
-          inediti diventando la prima a
-          <strong>
-            produrre e commercializzare giochi di qualità in italiano per
-            bambini musulmani
-          </strong>.
+          inediti diventando la prima azienda in Italia a produrre e
+          commercializzare giochi di qualità in italiano per bambini musulmani.
+          Tutti i nostri giochi sono conformi alla normativa vigente e sicuri
+          per i piccoli della nostra ummah. 
           <br />
-          Con l’ambizione dei suoi tre fondatori, Ummah Toys vuole essere
-          presente nelle camerette di ogni bambino e bambina musulmani. Tutti i
-          giocattoli sono certificati o prodotti secondo la normativa europea,
-          Ummah Toys ha deciso di mantenere un prezzo accessibile senza
-          compromettere la qualità, perché l’educazione dei nostri figli non è
-          un gioco!
+          <br />
+          Con l’ambizione dei suoi tre
+          fondatori, Ummah Toys vuole essere presente nelle camerette di ogni
+          bambino e bambina musulmani. UmmahToys è un alleato nella educazione
+          islamica dei tuoi figli ed è al servizio di : famiglie, moschee e
+          tutti gli operatori che si occupano di educazione islamica.
         </p>
       </main>
     </>
